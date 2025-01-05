022968b2ff2e9531ab623a7ea8f53afdcf6e7930
Marian Eickmeyer <mail@marianheinsen.com>
2025-01-05 01:37:21 +0100

    feat: add lib function to create bun package

--- lib.nix
+++ lib.nix
@@ -7,8 +7,49 @@ let
 
   allSystems = nixpkgs.lib.genAttrs systems;
 
+  buildBunPackage = (pkgs: {
+    pname,
+    version,
+    src,
+    hash,
+  }:
+    let
+      nodeModules = pkgs.stdenv.mkDerivation {
+        pname = "${pname}_node-modules";
+        inherit version src;
+
+        nativeBuildInputs = [ pkgs.bun ];
+        buildPhase = ''
+          bun install --production --no-progress --frozen-lockfile
+        '';
+
+        installPhase = ''
+          mkdir -p $out/node_modules
+          cp -R ./node_modules/* $out/node_modules
+        '';
+
+        outputHashAlgo = "sha256";
+        outputHashMode = "recursive";
+        outputHash = hash;
+      };
+    in pkgs.stdenv.mkDerivation {
+      inherit pname version src;
+      nativeBuildInputs = [ pkgs.bun nodeModules pkgs.makeBinaryWrapper ];
+
+      installPhase = ''
+        mkdir -p $out/
+        ln -s ${nodeModules}/node_modules $out/
+        cp -R ./src package.jso[n] tsconfig.jso[n] $out
+
+        makeBinaryWrapper ${pkgs.bun}/bin/bun $out/bin \
+          --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bun ]} \
+          --add-flags "run --prefer-offline --no-install $out/src/main.ts"
+      '';
+  });
+
   lib = {
     inherit allSystems;
+    inherit buildBunPackage;
   };
 in
   lib
