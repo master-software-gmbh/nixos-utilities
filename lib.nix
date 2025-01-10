a9bc193d70001a0d3d000c3ec1dcfb4f61ba6ad5
Marian Eickmeyer <mail@marianheinsen.com>
2025-01-10 13:19:21 +0100

    feat: add lib function to compile sqlite extensions

--- lib.nix
+++ lib.nix
@@ -54,12 +54,33 @@ let
       '';
   });
 
+  buildSqliteExtensions = (pkgs: path: let
+    compileExtension = if (pkgs.stdenv.hostPlatform.system == "aarch64-darwin") then "dylib" else "so";
+    compileFlag = if (pkgs.stdenv.hostPlatform.system == "aarch64-darwin") then "-dynamiclib" else "-shared";
+  in pkgs.stdenv.mkDerivation {
+    name = "sqlite-extensions";
+    nativeBuildInputs = [ pkgs.sqlite pkgs.gcc ];
+    src = path;
+
+    buildPhase = ''
+      for file in *.c; do
+        gcc -fPIC ${compileFlag} $file -o ''\${file%.c}.${compileExtension}
+      done
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp *.${compileExtension} $out
+    '';
+  });
+
   systemdServiceRef = name: "${name}.service";
 
   lib = {
     inherit allSystems;
     inherit buildBunDependencies;
     inherit buildBunPackage;
+    inherit buildSqliteExtensions;
     inherit systemdServiceRef;
   };
 in
