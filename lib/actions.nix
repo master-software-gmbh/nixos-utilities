f29da9bc352e833ac00fb981fe3bb5336fa5cec8
Marian Eickmeyer <mail@marianheinsen.com>
2025-03-18 08:39:18 +0100

    feat: add workflow template for semantic release

--- lib/actions.nix
+++ lib/actions.nix
@@ -2,12 +2,18 @@
   list = pkgs.writeShellScriptBin "actions-list" ''
     echo "actions-list"
     echo "actions-build-package"
+    echo "actions-create-release"
   '';
   buildPackage = pkgs.writeShellScriptBin "actions-build-package" ''
     mkdir -p ./.github/workflows
     cp ${./workflows/build-package.yaml} ./.github/workflows/build.yaml
   '';
+  createRelease = pkgs.writeShellScriptBin "actions-create-release" ''
+    mkdir -p ./.github/workflows
+    cp ${./workflows/create-release.yaml} ./.github/workflows/release.yaml
+  '';
 in [
   list
   buildPackage
+  createRelease
 ]
