1fdeb7ab4543329b561dfa7ac2cea09641aec6d3
Marian Eickmeyer <mail@marianheinsen.com>
2025-05-29 14:53:58 +0200

    feat: make eric derivation support macos

--- lib/eric.nix
+++ lib/eric.nix
@@ -1,16 +1,26 @@
 { ... }: {
-  buildEric41 = (pkgs: { plugins ? [] }: pkgs.stdenv.mkDerivation {
+  buildEric41 = (pkgs: { plugins ? [] }: let
+    version = "41.6.2.0";
+    sources = {
+      "aarch64-darwin" = pkgs.fetchurl {
+        url = "https://download.elster.de/download/eric/eric_41/ERiC-${version}-Darwin-universal.jar";
+        hash = "sha256-wxfuQOalmWPKG8z4Vc+bGN8R5v0YZCUAH/ZHXs24yQ4=";
+      };
+      "x86_64-linux" = pkgs.fetchurl {
+        url = "https://download.elster.de/download/eric/eric_41/ERiC-${version}-Linux-x86_64.jar";
+        hash = "sha256-zHitG4Ktt+iCKk9GrC3C4MRSWhUxh89kW9bUeHzqNJs=";
+      };
+    };
+
+  in pkgs.stdenvNoCC.mkDerivation {
     name = "eric";
-    version = "41.5.4";
+    inherit version;
 
     nativeBuildInputs = [
       pkgs.unzip
     ];
 
-    src = pkgs.fetchurl {
-      url = https://download.elster.de/download/eric/eric_41/ERiC-41.5.4.0-Linux-x86_64.jar;
-      sha256 = "sha256-8GZfBYOC6gfFmIV1B2QuklEDadA4XrsYYyryCcAg8h0=";
-    };
+    src = sources.${pkgs.stdenvNoCC.hostPlatform.system} or (throw "Unsupported system: ${pkgs.stdenvNoCC.hostPlatform.system}");
 
     unpackPhase = ''
       unzip $src
@@ -19,13 +29,14 @@
     installPhase = ''
       mkdir -p $out/plugins2
 
-      mv ERiC-41.5.4.0/Linux-x86_64/lib/libericapi.so $out/
-      mv ERiC-41.5.4.0/Linux-x86_64/lib/libeSigner.so $out/
-      mv ERiC-41.5.4.0/Linux-x86_64/lib/libericxerces.so $out/
-      mv ERiC-41.5.4.0/Linux-x86_64/lib/plugins2/libcommonData.so $out/plugins2/
+      # Move files with .so extension for Linux or .dylib extension for macOS
+      mv ERiC-${version}/*/lib/libericapi.{so,dylib} $out/
+      mv ERiC-${version}/*/lib/libeSigner.{so,dylib} $out/
+      mv ERiC-${version}/*/lib/libericxerces.{so,dylib} $out/
+      mv ERiC-${version}/*/lib/plugins2/libcommonData.{so,dylib} $out/plugins2/
 
       for plugin in ${builtins.concatStringsSep " " plugins}; do
-        mv ERiC-41.5.4.0/Linux-x86_64/lib/plugins2/$plugin.so $out/plugins2/
+        mv ERiC-${version}/*/lib/plugins2/$plugin.{so,dylib} $out/plugins2/
       done
     '';
   });
